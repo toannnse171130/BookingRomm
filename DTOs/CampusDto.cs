cb8cb33690b703901728a4fd00c98b7a44438239
toannnse171130 <toannnse171130@fpt.edu.vn>
2025-12-13 21:45:17 +0700

    Create CampusDto.cs

---
+++ DTOs/CampusDto.cs
@@ -0,0 +1,10 @@
+namespace FPT_Booking_BE.DTOs
+{
+    public class CampusDto
+    {
+        public int CampusId { get; set; }
+        public string CampusName { get; set; } = string.Empty;
+        public string? Address { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
